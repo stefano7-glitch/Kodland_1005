c298af157608c17d03109266f1eff3f0cbd853ed
stefano7-glitch <scuola.stefanobruno@icsabatorino.edu.it>
2025-07-19 12:00:08 +0200

    Create classe_bot.py
    salvo il mio primo bot

---
+++ classe_bot.py
@@ -0,0 +1,27 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Hai fatto l\'accesso come {bot.user}')
+
+@bot.command()
+async def ciao(ctx):
+    await ctx.send(f'Ciao! Sono un bot {bot.user}!')
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def add(ctx, left: int, right: int):
+    """Adds two numbers together."""
+    await ctx.send(left + right)
+    
+
+bot.run("Inserisci il token")
